3731b5cd4703b57edfb578252bff1f6f09f44a29
Tyler Jass <jasstsg@gmail.com>
2024-10-09 00:11:41 -0400

    basic theme and high level structure

---
+++ src/components/body/Body.tsx
@@ -0,0 +1,21 @@
+import './body.scss';
+
+function Body() {
+    return (
+        <div className="body">
+            <div className="h-line"></div>
+            <div>
+                Thank you for your interest in me!  Unfortunately this site is still under construction.  Please check back later.
+            </div>
+            <div className="h-line"></div>
+            <section id="projects">Projects</section>
+            <div className="h-line"></div>
+            <section id="work">Work Experience</section>
+            <div className="h-line"></div>
+            <section id="education">Education</section>
+            <div className="h-line"></div>
+        </div>
+    );
+}
+
+export default Body;
---
+++ src/components/header/Header.tsx
@@ -0,0 +1,18 @@
+import './header.scss';
+
+function Header() {
+    return (
+        <div className="header">
+            <nav>
+                <ul>
+                    <li><a href="/">Tyler Jass</a> </li>
+                    <li><a href="#projects">Projects</a></li>
+                    <li><a href="#work">Work</a></li>
+                    <li><a href="#education">Education</a></li>
+                </ul>
+            </nav>
+        </div>
+    );
+}
+
+export default Header;
